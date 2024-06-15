f1b8e38e295f4636f54f03151e21c314b49ee1c6
Pornchai <pornchai.chy@gmail.com>
2024-06-15 22:08:44 +0700

    Update test_requests.py

--- test_requests.py
+++ test_requests.py
@@ -1,7 +1,7 @@
 import requests
 r = requests.post('http://127.0.0.1:5000/webhook/binance_future', 
                         json={
-                                "ACTION" : "TPSL SHORT",
+                                "ACTION" : "OPEN LONG",
                                 "SYMBOL" : "BTCUSDT",
                                 "AMOUNT" : 0.09,
                         })
