61a2165aa36e67eb668bd59db9f58a4b75732d82
Pornchai <pornchai.chy@gmail.com>
2024-07-27 16:43:04 +0700

    xxx

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def webhook_binance_future():
                
         elif "TPSLL" in action:
                 order_tpsl_long(symbol,amount)
-                txt_tpsl_long = "【ส่งคำสั่ง】TP/SL LONG : {} จำนวนสัญญา : {}".format(symbol,amount)
+                txt_tpsl_long = "【ส่งคำสั่ง】TP LONG : {} จำนวนสัญญา : {}".format(symbol,amount)
                 print(txt_tpsl_long)
                 notify.send(txt_tpsl_long)
                 # เรียกฟังก์ชั่นในการปิดสัญญาLONG ส่งไปที่ Exchange , Broker
@@ -68,7 +68,7 @@ def webhook_binance_future():
                
         elif "TPSLS" in action:             
                 order_tpsl_short(symbol,amount)
-                txt_tpsl_short = "【ส่งคำสั่ง】TP/SL SHORT : {} จำนวนสัญญา : {}".format(symbol,amount)
+                txt_tpsl_short = "【ส่งคำสั่ง】TP SHORT : {} จำนวนสัญญา : {}".format(symbol,amount)
                 print(txt_tpsl_short)
                 notify.send(txt_tpsl_short)
                 # เรียกฟังก์ชั่นในการปิดสัญญาSHORT ส่งไปที่ Exchange , Broker
